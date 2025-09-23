4b9781e4d6e13ff50092ce45142df227332fe2db
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-09-22 21:08:20 -0300

    dialectModule: mysql,
        dialectModule: mysql,

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const mysql = require('mysql2');            // <= ADICIONE ESTA LINHA
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -10,22 +11,37 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // se você usar URL única (DATABASE_URL), ainda assim força mysql
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
+    dialect: 'mysql',
+    dialectModule: mysql,                   // <= FORÇA MYSQL
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      ...config,
+      dialect: 'mysql',
+      dialectModule: mysql,                 // <= FORÇA MYSQL
+    }
+  );
 }
 
+// resto do arquivo permanece igual
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
+  .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
