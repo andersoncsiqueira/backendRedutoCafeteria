998be4fc8f27333943c433063c9102f0d70f5bcc
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-09-22 21:17:10 -0300

    mais um

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const mysql = require('mysql2');            // <= ADICIONE ESTA LINHA
+const { Sequelize } = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -11,43 +10,30 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  // se você usar URL única (DATABASE_URL), ainda assim força mysql
   sequelize = new Sequelize(process.env[config.use_env_variable], {
-    ...config,
     dialect: 'mysql',
-    dialectModule: mysql,                   // <= FORÇA MYSQL
+    logging: false,
   });
 } else {
   sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
-    {
-      ...config,
-      dialect: 'mysql',
-      dialectModule: mysql,                 // <= FORÇA MYSQL
-    }
+    { ...config, dialect: 'mysql', logging: false }
   );
 }
 
-// resto do arquivo permanece igual
-fs
-  .readdirSync(__dirname)
-  .filter((file) => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach((file) => {
+// LOG TEMPORÁRIO para confirmar no PM2:
+console.log('DB DIALECT AT RUNTIME:', sequelize.getDialect());
+
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+  .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
+Object.keys(db).forEach(name => db[name].associate && db[name].associate(db));
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
